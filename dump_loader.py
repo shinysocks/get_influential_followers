d9d8f34df2ae7aad5214bb60f2a08d2f2dd87dd2
Timothy Faust <tim.faust@gmail.com>
2025-11-24 16:06:18 -0600

    parse multiple dumps

--- dump_loader.py
+++ dump_loader.py
@@ -1,16 +1,19 @@
 from os import system
+import glob
 import json
 
-dump = 'followers_1.json'
+file_list = glob.glob('followers*.json')
+
 output = 'followers.txt'
 users = []
 
-with open(dump, 'r', encoding='utf-8') as f:
-    dump = json.load(f)
+for file in file_list:
+    with open(file, 'r', encoding='utf-8') as f:
+        dump = json.load(f)
 
-for item in dump:
-    users.append(item['string_list_data'][0]['value'])
-f.close()
+    for item in dump:
+        users.append(item['string_list_data'][0]['value'])
+    f.close()
 
 with open(output, "w") as file:
     for item in users:
